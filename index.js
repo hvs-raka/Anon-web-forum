bd47f8e013c48647f1d33a47d4ad01a0bc5ae949
Raka <harshveerraka@gmail.com>
2024-04-08 10:56:36 +0530

    making connection with front-end
    not done yet

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer();
+const io = new Server(server);
 
-let port = 9000;
+//socket io
+io.on("connection", (socket) => {
+  socket.on("connection", () => {
+    console.log("Front-end is connected");
+  });
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
+
+const port = process.env.PORT || 9000;
 server.listen(port, () => {
-  console.log(`Server is running at ${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
